d92c0b21e77e1e332ebad68c1d9ec79c4e71b32b
Shaquibkhan <shaquibkhan2003@gmail.com>
2024-07-09 23:42:29 +0530

    New Changes

--- src/api.js
+++ src/api.js
@@ -3,7 +3,8 @@ export const geoApiOptions = {
 	headers: {
 		'x-rapidapi-key': '8d2a110b6ad468ae1a0e459757cf659d',
 		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
-	}
+	}, referrerPolicy: "unsafe-url" 
+
 };
 // export const GEO_API_URL = "http://api.openweathermap.org/data/2.5/weather";
 
